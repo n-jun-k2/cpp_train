fc326a9cb4682c5f21a99c647ad408780c370c0b
jun-pc <http.kj.com@gmail.com>
2021-05-16 16:27:47 +0900

    update　出力値をコメントに追記

--- cpp/pool_pattern/src/main.cpp
+++ cpp/pool_pattern/src/main.cpp
@@ -28,8 +28,8 @@ std::shared_ptr<std::shared_ptr<Point>> build(Pool<Point>& pointPool) {
 
 int main() {
 
-  std::cout << "Point size: " << sizeof(Point) << std::endl;
-  std::cout << "Resource<Point> size: " << sizeof(Resource<Point>) << std::endl;
+  std::cout << "Point size: " << sizeof(Point) << std::endl; //Point size: 16
+  std::cout << "Resource<Point> size: " << sizeof(Resource<Point>) << std::endl; // Resource<Point> size: 16
 
   Resource<Point> item;
   Point point = static_cast<Point>(item.live);
@@ -37,30 +37,60 @@ int main() {
   Pool<Point, std::vector> pointpool(5);
   for (auto buffer : pointpool.buffer.wrapper) {
     std::cout << "buffer :" << buffer.next << " : " << buffer.live.x << "," << buffer.live.y << std::endl;
+    // buffer :0x55eb69072a60 : 4.66742e-310,0
+    // buffer :0x55eb69072a70 : 4.66742e-310,0
+    // buffer :0x55eb69072a80 : 4.66742e-310,0
+    // buffer :0x55eb69072a90 : 4.66742e-310,0
+    // buffer :0 : 0,0
   }
 
   std::cout << "------" << std::endl;
   for (int i = 0; i < pointpool.buffer.wrapper.size(); ++i) {
     std::cout << "adder :" << &pointpool.buffer.wrapper[i] << std::endl;
+    // adder :0x55eb69072a50
+    // adder :0x55eb69072a60
+    // adder :0x55eb69072a70
+    // adder :0x55eb69072a80
+    // adder :0x55eb69072a90
   }
 
   std::cout << "------" << std::endl;
   for (auto pointer = pointpool.firstAvailable; pointer != nullptr; pointer = pointer->next) {
     std::cout << "adder :" << pointer << std::endl;
+    // adder :0x55eb69072a50
+    // adder :0x55eb69072a60
+    // adder :0x55eb69072a70
+    // adder :0x55eb69072a80
+    // adder :0x55eb69072a90
   }
 
   std::cout << "------" << std::endl;
   {
     auto pPoint = build(pointpool);
 
+    std::cout << "point : " << (*pPoint)->x <<","<< (*pPoint)->y << std::endl;
+
     for (auto pointer = pointpool.firstAvailable; pointer != nullptr; pointer = pointer->next) {
       std::cout << "adder :" << pointer << std::endl;
     }
+    // create instance pointer...
+    // point : -1,1
+    // adder :0x55eb69072a60
+    // adder :0x55eb69072a70
+    // adder :0x55eb69072a80
+    // adder :0x55eb69072a90
+    // delete pointer...
+    // release...
   }
 
   std::cout << "------" << std::endl;
   for (auto pointer = pointpool.firstAvailable; pointer != nullptr; pointer = pointer->next) {
     std::cout << "adder :" << pointer << std::endl;
+    // adder :0x55eb69072a50
+    // adder :0x55eb69072a60
+    // adder :0x55eb69072a70
+    // adder :0x55eb69072a80
+    // adder :0x55eb69072a90
   }
 
   {
@@ -79,11 +109,21 @@ int main() {
     for (auto pointer = pointpool.firstAvailable; pointer != nullptr; pointer = pointer->next) {
       std::cout << "adder :" << pointer << std::endl;
     }
+    // empty
+    // release...
+    // release...
+    // release...
+    // release...
+    // release...
   }
 
   std::cout << "------" << std::endl;
   for (auto pointer = pointpool.firstAvailable; pointer != nullptr; pointer = pointer->next) {
     std::cout << "adder :" << pointer << std::endl;
+    // adder :0x55eb69072a50
+    // adder :0x55eb69072a60
+    // adder :0x55eb69072a70
+    // adder :0x55eb69072a80
   }
 
   return 0;
