2d9b02f4bca1975efafc6006b749cc9681086023
jun-pc <http.kj.com@gmail.com>
2022-01-06 19:21:39 +0900

    update 配列の配列

--- cpp/tmptmp/src/main.cpp
+++ cpp/tmptmp/src/main.cpp
@@ -4,115 +4,158 @@
 #include <vector>
 #include <string>
 #include <functional>
+#include <variant>
 
+using instance_type = std::variant<std::vector<CLASS_A>, std::vector<CLASS_B>>;
+template<class> struct range;
+template<class R, class... ArgTypes>
+struct range <R(ArgTypes...)>
+{const unsigned int size; std::function<R(ArgTypes...)> func;};
+template<class T>
+using vector_range = range<void(std::vector<T>&)>;
 
-
-struct CreateCObject {
-  CLASS_C m_instanceC;
-  std::vector<CLASS_A> m_instanceA;
-  std::vector<CLASS_B> m_instanceB;
-
-  struct update{};
-  struct type {int value;};
-  struct priority {float value;};
-
-  CreateCObject& operator<<(type&& type){
-    m_instanceC.type = type.value;
-    return *this;
-  }
-  CreateCObject& operator<<(priority&& priority) {
-    m_instanceC.priority = priority.value;
-    return *this;
-  }
-  CreateCObject& operator<<(std::function<void(std::vector<CLASS_A>&)> update) {
-    update(m_instanceA);
-    return *this;
-  }
-  CreateCObject& operator<<(std::function<void(std::vector<CLASS_B>&)> update) {
-    update(m_instanceB);
-    return *this;
-  }
-  void operator<<(const update _) {
-    m_instanceC.a_count = m_instanceA.size();
-    m_instanceC.p_a = m_instanceA.data();
-    m_instanceC.b_count = m_instanceB.size();
-    m_instanceC.p_b = m_instanceB.data();
-  }
-};
-
-CreateCObject::type operator""Type (unsigned long long v) {
-  return CreateCObject::type{static_cast<int>(v)};
-}
-
-CreateCObject::priority operator""Priority (long double v) {
-  return CreateCObject::priority{static_cast<float>(v)};
-}
-
-std::string get_string() {
-  return std::string("function return.");
-}
+struct CREATER;
 
 template<class T>
-class TextDisplayer {
-  public:
-    explicit TextDisplayer(T&& text)
-    :text_(std::forward<T>(text)){}
-  private:
-    const T text_;
+class IWRAP {
+protected:
+  T& obj;
+public:
+  explicit IWRAP(T& b): obj(b){}
 };
 
-// deduction guide
-template<class T>TextDisplayer(T&&) -> TextDisplayer<T>;
+class CLASS_A_WRAP : public IWRAP<CLASS_A>{
+public:
+  using IWRAP<CLASS_A>::IWRAP;
+  void update() {
+    this->obj.x = 10;
+    this->obj.y = 5;
+    this->obj.z = 1;
+  }
+};
+class CLASS_B_WRAP : public IWRAP<CLASS_B>{
+public:
+  using IWRAP<CLASS_B>::IWRAP;
+  void update(){
+    this->obj.r = 0.5f;
+    this->obj.g = 0.1f;
+    this->obj.b = 2.5f;
+  }
+};
+
+class CLASS_C_WRAP {
+  private:
+    CREATER& m_obj;
+    CLASS_C& m_c;
+    instance_type& m_instance_type;
+  private:
+    template<class TWrap, class T>
+    void __range_func(vector_range<TWrap> create) {
+      auto row_array = std::vector<T>(create.size);
+      auto wrap_array = std::vector<TWrap>();
+      for(auto i = 0; i < create.size; ++i)
+        wrap_array.emplace_back(row_array[i]);
+      create.func(wrap_array);
+      m_instance_type = row_array;
+    }
+    struct visitor {
+      CLASS_C& m_c;
+      void operator()(std::vector<CLASS_A>& a) {
+        m_c.a_count = a.size();
+        m_c.p_a = a.data();
+      }
+      void operator()(std::vector<CLASS_B>& b) {
+        m_c.b_count = b.size();
+        m_c.p_b = b.data();
+      }
+    };
+
+  public:
+    struct update{};
+    struct type {int value;};
+    struct priority {float value;};
+
+    explicit CLASS_C_WRAP(CREATER& a, CLASS_C& b, instance_type& c)
+    :m_obj(a), m_c(b), m_instance_type(c){}
+
+    CLASS_C_WRAP& operator<<(type&& type){
+      m_c.type = type.value;
+      return *this;
+    }
+    CLASS_C_WRAP& operator<<(priority&& priority) {
+      m_c.priority = priority.value;
+      return *this;
+    }
+    CLASS_C_WRAP& operator<<(const vector_range<CLASS_A_WRAP>& range){
+      __range_func<CLASS_A_WRAP, CLASS_A>(range);
+      return *this;
+    }
+    CLASS_C_WRAP& operator<<(const vector_range<CLASS_B_WRAP>& range){
+      __range_func<CLASS_B_WRAP, CLASS_B>(range);
+      return *this;
+    }
+    CREATER& operator<<(const update _) {
+      std::visit(visitor{m_c}, m_instance_type);
+      return m_obj;
+    }
+};
+
+struct CREATER {
+  std::vector<CLASS_C> m_instanceC;
+  std::vector<instance_type> m_instance;
+
+  CREATER& operator<<(const vector_range<CLASS_C_WRAP>& _range) {
+    m_instanceC.resize(_range.size);
+    m_instance.resize(_range.size);
+
+    std::vector<CLASS_C_WRAP> items;
+    for(auto i = 0; i < _range.size; ++i)
+      items.emplace_back(*this, m_instanceC[i], m_instance[i]);
+
+    _range.func(items);
+    return *this;
+  }
+
+};
+
+CLASS_C_WRAP::type operator""_Type (unsigned long long v) {
+  return CLASS_C_WRAP::type{static_cast<int>(v)};
+}
+
+CLASS_C_WRAP::priority operator""_Priority (long double v) {
+  return CLASS_C_WRAP::priority{static_cast<float>(v)};
+}
+
+
 
 int main() {
-
-  CreateCObject instance = {};
-  instance
-    << 10Type
-    << 0.5Priority
-    << [](std::vector<CLASS_B>& vec){
-      const int size = 10;
-      vec.resize(size);
-      for(auto& v : vec) {
-        v.b = 0.5f;
-        v.r = 0.8f;
-        v.g = 1.0f;
+  unsigned int c_array_size = 10;
+  CREATER factory = {};
+  factory << vector_range<CLASS_C_WRAP> {
+    c_array_size,
+    [=](std::vector<CLASS_C_WRAP>& c_array)
+    {
+      for(int i = 0; i < c_array_size; ++i) {
+        auto c = c_array[i];
+        if (i % 2 == 0)
+          c << 10_Type << 0.4_Priority << vector_range<CLASS_A_WRAP> {5, [](std::vector<CLASS_A_WRAP>& a_array){
+            for(auto& a : a_array)a.update();
+          }}
+          << CLASS_C_WRAP::update();
+        else
+          c << 6_Type << 0.1_Priority << vector_range<CLASS_B_WRAP> {3, [](std::vector<CLASS_B_WRAP>& b_array){
+            for(auto& b : b_array) b.update();
+          }}
+          << CLASS_C_WRAP::update();
       }
     }
-    << [](std::vector<CLASS_A>& vec){
-      const int size = 5;
-      vec.resize(size);
-      for(auto& v : vec) {
-        v.x = 1;
-        v.y = 2;
-        v.z = 3;
-      }
-    }
-    << CreateCObject::update();
+  };
 
-  std::cout << instance.m_instanceC.type << std::endl;
-  std::cout << instance.m_instanceC.priority << std::endl;
-  std::cout << instance.m_instanceC.a_count << std::endl;
-  for (auto i = 0; i < instance.m_instanceC.a_count; ++i) {
-    auto a = instance.m_instanceC.p_a[i];
-    std::cout << a.x
-              << ","
-              << a.y
-              << ","
-              << a.z
-              << std::endl;
-  }
-  std::cout << instance.m_instanceC.b_count << std::endl;
-  for (auto i = 0; i < instance.m_instanceC.b_count; ++i) {
-    auto b = instance.m_instanceC.p_b[i];
-    std::cout << b.b << ","
-              << b.g << ","
-              << b.r
-              << std::endl;
+  auto& c_array = factory.m_instanceC;
+  int i = 0;
+  for (auto& c : c_array) {
+    std::cout << ++i << ": type :" << c.type << ", a_count :" << c.a_count << ", b_count :" << c.b_count << std::endl;
   }
 
-  std::string txt = "Hello World";
-  TextDisplayer display1(txt);
-  TextDisplayer display2(get_string());
   return 0;
 }
