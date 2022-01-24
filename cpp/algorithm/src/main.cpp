9c81fcdcbc04cb5e1c828423e9a06d5f4d961cf8
jun-pc <http.kj.com@gmail.com>
2022-01-24 19:13:11 +0900

    update algorithm 不要な変数削除＋定義名修正

--- cpp/algorithm/src/main.cpp
+++ cpp/algorithm/src/main.cpp
@@ -33,51 +33,46 @@ class eps_greedy {
     double EPS;
     std::random_device device;
     std::ranlux48 engine;
+    std::vector<ST> ACTION_ALL;
     std::unordered_map<ST, RT>& Q;
     std::uniform_real_distribution<> r_dist;
     std::uniform_int_distribution<> i_dist;
   public:
-    eps_greedy(std::unordered_map<ST, RT>& q, const double eps, const int size)
-    :Q(q), EPS(eps), device(), engine(device()), i_dist(0, size), r_dist(0.0, 1.0)
+    eps_greedy(std::unordered_map<ST, RT>& q, const double eps, std::vector<ST> actions)
+    :Q(q), EPS(eps), device(), engine(device()), ACTION_ALL(actions), i_dist(0, ACTION_ALL.size()), r_dist(0.0, 1.0)
     {}
 
     ST operator()() {
       auto max_idx = std::distance(std::begin(Q), std::max_element(std::begin(Q), std::end(Q), [](const auto& a, const auto& b){return a.second > b.second;}));
       if (EPS > r_dist(engine))
-        return i_dist(engine);
+        return ACTION_ALL[i_dist(engine)];
       else
         return static_cast<ST>(max_idx);
     }
 };
 
-template<class ST, class RT, template<class T, class A = std::allocator<T>>class CONTAINER>
+template<class ST, class RT>
 class ucb_1 {
   private:
     double inf;
     unsigned long long N;
+    std::vector<ST> ACTION_ALL;
     std::unordered_map<ST, unsigned long long> NJ;
     std::unordered_map<ST, double> scores;
     std::unordered_map<ST, RT>& X;
 
   public:
 
-    ucb_1(std::unordered_map<ST, RT>& q, const double inf, CONTAINER<ST> init)
-    :X(q), inf(inf), N(0) {
-      for(const auto& k : init) {
-        NJ[k] = 0.0;
-        scores[k] = 0.0;
-      }
-    }
+    ucb_1(std::unordered_map<ST, RT>& q, const double inf, const std::vector<ST>& actions)
+    :X(q), inf(inf), N(0), ACTION_ALL(actions) {}
 
     ST operator()() {
-      const auto inc = 1.0;
-      const auto is_inf = 0.0;
-      const auto idx_size = X.size();
 
       /* select all options once. */
-      if (N <= idx_size) return N++;
+      if (N < ACTION_ALL.size()) return ACTION_ALL[N++];
 
       ++N;
+      const auto idx_size = X.size();
       for (auto idx = 0; idx < idx_size; ++idx)
         scores[idx] =  X[idx] + std::sqrt(2.0 * std::log(N) / (NJ[idx] + inf));
 
@@ -107,7 +102,7 @@ class Bandit {
           return std::normal_distribution<_REAL_TYPE>(_mean(engine), _dist(engine));
         });
 
-      this->output(std::cout);
+      this->output_state(std::cout);
     }
 
     _REAL_TYPE operator()(_STATE_TYPE i) {
@@ -115,26 +110,25 @@ class Bandit {
       return array_normal[i](engine);
     }
 
-    std::ostream& output(std::ostream& os) {
+    void output_state(std::ostream& os) {
       os << "=============" << std::endl;
       int i = 0;
       for (const auto& n : array_normal) {
         os << std::fixed << std::setprecision(2) <<"[" << i++ << "] mean=" << n.mean() << "," << "sigma=" << n.stddev() << std::endl;
       }
       os << "=============" << std::endl;
-      return os;
     }
 };
 
 /*強化学習用フロー*/
 struct ReinforcementLearning {
-  template<class ST, class RT>
+  template<class AT, class RT>
   static void For(
-    std::function<ST()> choose_action,
-    std::function<RT(ST)> choose_reward,
-    std::function<bool(RT, ST, unsigned long long)> is_end) {
+    std::function<AT()> choose_action,
+    std::function<RT(AT)> choose_reward,
+    std::function<bool(RT, AT, unsigned long long)> is_end) {
       unsigned long long idx = 0;
-      ST action;
+      AT action;
       RT reward;
       do {
         action = choose_action();
@@ -179,23 +173,20 @@ int main() {
   }
   { // 多腕バンディット問題のサンプル
     using REWARD_TYPE = double;
-    using STATE_TYPE = int;
+    using ACTION_TYPE = int;
 
     constexpr int BANDIT_SIZE = 10;
     constexpr unsigned long long FOR_COUNT = 100;
 
-    std::unordered_map<STATE_TYPE, REWARD_TYPE> Q;
-    auto env = Bandit<BANDIT_SIZE, std::mt19937, STATE_TYPE, REWARD_TYPE>(0, 10, -1, 1);
-    // auto policy = eps_greedy<STATE_TYPE, REWARD_TYPE>(Q, 0.8, BANDIT_SIZE);
+    std::unordered_map<ACTION_TYPE, REWARD_TYPE> Q;
+    auto env = Bandit<BANDIT_SIZE, std::mt19937, ACTION_TYPE, REWARD_TYPE>(0, 10, -1, 1);
+    auto policy = eps_greedy(Q, 0.8, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+    // auto policy = ucb_1(Q, 0.01, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 
-    std::vector<STATE_TYPE> init_key(BANDIT_SIZE);
-    std::iota(std::begin(init_key), std::end(init_key), 0);
-    auto policy = ucb_1(Q, 0.01, init_key);
-
-    ReinforcementLearning::For<STATE_TYPE, REWARD_TYPE>(
+    ReinforcementLearning::For<ACTION_TYPE, REWARD_TYPE>(
       [&](){return policy();},
-      [&](STATE_TYPE T){return env(T);},
-      [&](REWARD_TYPE r, STATE_TYPE s, unsigned long long i) {
+      [&](ACTION_TYPE T){return env(T);},
+      [&](REWARD_TYPE r, ACTION_TYPE s, unsigned long long i) {
         // update.
         const double INC = 1.0;
         auto mean = mean_1_14(Q[s], r, Q[s], Q[s] + INC);
