f2babc2107872cbf0ddb8bd1b83a7f727ef6ac6b
jun-pc <http.kj.com@gmail.com>
2022-02-15 20:48:14 +0900

    update algorithm lambda対応

--- cpp/algorithm/src/main.cpp
+++ cpp/algorithm/src/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <memory>
 #include <random>
+#include <map>
 #include <unordered_map>
 #include <numeric>
 #include <algorithm>
@@ -27,76 +28,73 @@ double mean_1_15(const double _X, const double x, const double t, const double N
   return ((current - 1.0) / current) * _X + (1.0 / current) * x;
 }
 
-template<class AT, class RT, class QT>
+double mean_1_20(const double _X, const double a, const double v) {
+  return _X + (a * (v - _X));
+}
+
+template<class AT>
 class eps_greedy {
+  public:
+    using MAX_FUNC = std::function<AT()>;
+    using RANDOM_FUNC = std::function<AT(const double)>;
   private:
-    QT& Q;
     double EPS;
     std::random_device device;
     std::ranlux48 engine;
-    std::vector<AT> ACTION_ALL;
     std::uniform_real_distribution<> r_dist;
-    std::uniform_int_distribution<> i_dist;
+    MAX_FUNC max;
+    RANDOM_FUNC random;
   public:
-    eps_greedy(QT& q, const double eps, std::vector<AT> actions)
-    :Q(q), EPS(eps), device(), engine(device()), ACTION_ALL(actions), i_dist(0, ACTION_ALL.size()), r_dist(0.0, 1.0)
-    {}
+    eps_greedy(const double eps, MAX_FUNC get_max, RANDOM_FUNC get_random)
+    : EPS(eps), device(), engine(device()), r_dist(0.0, 1.0), max(get_max), random(get_random){}
 
     AT operator()() {
-      auto max_idx = std::distance(std::begin(Q), std::max_element(std::begin(Q), std::end(Q), [](const auto& a, const auto& b){return a.second > b.second;}));
       if (EPS > r_dist(engine))
-        return ACTION_ALL[i_dist(engine)];
+        return random(r_dist(engine));
       else
-        return static_cast<AT>(max_idx);
+        return max();
     }
 };
 
-template<class AT, class RT, class QT>
+template<class ST>
 class ucb_1 {
+  public:
+    using real_type = double;
+    using integer_type = unsigned int;
+    using value_func = std::function<real_type(integer_type)>;
+    using is_order_func = std::function<bool(integer_type)>;
+    using order_action_func = std::function<ST(integer_type)>;
   private:
-    double inf;
-    unsigned long long N;
-    std::vector<AT> ACTION_ALL;
-    std::unordered_map<AT, unsigned long long> NJ;
-    std::unordered_map<AT, double> scores;
-    QT& X;
+    real_type inf;
+    integer_type N;
+    std::unordered_map<ST, integer_type> NJ;
+    std::unordered_map<ST, real_type> scores;
+
+    value_func get_value;
+    is_order_func is_order;
+    order_action_func order_action;
 
   public:
 
-    ucb_1(QT& q, const double inf, const std::vector<AT>& actions)
-    :X(q), inf(inf), N(0), ACTION_ALL(actions) {}
+    ucb_1(const real_type inf, value_func get_value, is_order_func is_order, order_action_func order_action)
+    :inf(inf), get_value(get_value), is_order(is_order), order_action(order_action), N(0) {}
 
-    AT operator()() {
+    ST operator()() {
 
       /* select all options once. */
-      if (N < ACTION_ALL.size()) return ACTION_ALL[N++];
+      if (is_order(N)) return order_action(N++);
 
-      ++N;
-      const auto idx_size = X.size();
-      for (auto idx = 0; idx < idx_size; ++idx)
-        scores[idx] =  X[idx] + std::sqrt(2.0 * std::log(N) / (NJ[idx] + inf));
+      for (auto idx = 0; is_order(idx); ++idx)
+        scores[idx] =  get_value(idx) + std::sqrt(2.0 * std::log(N) / (NJ[idx] + inf));
 
       auto max_itr = std::max_element(std::begin(scores), std::end(scores), [](const auto& a, const auto& b){return a.second > b.second;});
       auto max_idx = std::distance(std::begin(scores), max_itr);
       NJ[max_idx] += 1.0;
 
-      return max_idx;
+      return order_action(max_idx);
     }
 };
 
-template<class AT, class RT, class QT>
-class BayesSampling {
-  private:
-    QT& Q;
-    std::vector<AT> ACTION_ALL;
-  public:
-    BayesSampling(QT& q, const std::vector<AT>& actions)
-    : Q(q), ACTION_ALL(actions) {}
-
-    AT operator()() {
-
-    }
-};
 
 /*多腕バンディット問題*/
 template<int _SIZE, class _ENGIN_TYPE, class _STATE_TYPE=int, class _REAL_TYPE = float,
@@ -136,18 +134,19 @@ class Bandit {
 
 /*強化学習用フロー*/
 struct ReinforcementLearning {
+  using INDEX = unsigned long long;
   template<class AT, class RT>
   static void For(
-    std::function<AT()> choose_action,
-    std::function<RT(AT)> choose_reward,
-    std::function<bool(RT, AT, unsigned long long)> is_end) {
-      unsigned long long idx = 0;
-      AT action;
-      RT reward;
-      do {
-        action = choose_action();
-        reward = choose_reward(action);
-      }while(!is_end(reward, action, idx++));
+    const std::function<AT(INDEX, RT)> choose_action,
+    const std::function<RT(INDEX, AT)> choose_reward,
+    const std::function<bool(INDEX, RT, AT)> is_end) {
+    INDEX idx = 0;
+    AT action;
+    RT reward;
+    do {
+      action = choose_action(idx, reward);
+      reward = choose_reward(idx, action);
+    } while(!is_end(idx++, reward, action));
   }
 };
 
@@ -185,23 +184,45 @@ int main() {
       std::cout << "mean:" << mean << std::endl;
     }
   }
-  { // 多腕バンディット問題のサンプル
+  { // 多腕バ ンディット問題のサンプル
     using REWARD_TYPE = double;
     using ACTION_TYPE = int;
     using VALUE_FUNC_TYPE = std::unordered_map<ACTION_TYPE, REWARD_TYPE>;
 
     constexpr int BANDIT_SIZE = 10;
     constexpr unsigned long long FOR_COUNT = 100;
+    constexpr double EPS = 0.3;
 
     VALUE_FUNC_TYPE Q;
-    auto env = Bandit<BANDIT_SIZE, std::mt19937, ACTION_TYPE, REWARD_TYPE>(0, 10, -1, 1);
-    // auto policy = eps_greedy<ACTION_TYPE, REWARD_TYPE, VALUE_FUNC_TYPE>(Q, 0.8, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-    auto policy = ucb_1<ACTION_TYPE, REWARD_TYPE, VALUE_FUNC_TYPE>(Q, 0.01, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+    auto env = Bandit<BANDIT_SIZE, std::mt19937, ACTION_TYPE, REWARD_TYPE>(0, BANDIT_SIZE, -1, 1);
+    // auto policy = eps_greedy<int>{
+    //   EPS,
+    //   [&]() -> int{
+    //     auto max_itr = std::max_element(std::begin(Q), std::end(Q),
+    //       [](const auto& a, const auto& b) {return a.second > b.second;});
+    //     int max_idx = std::distance(std::begin(Q), max_itr);
+    //     return max_idx;
+    //   },
+    //   [&](const double r) -> int{
+    //     auto idx = static_cast<int>((BANDIT_SIZE) * r);
+    //     return idx;
+    //   }};
+    auto policy = ucb_1<ACTION_TYPE>(
+      0.01,
+      [&](unsigned int a){
+        return Q[a];
+      },
+      [BANDIT_SIZE](unsigned int n){
+        return n < BANDIT_SIZE;
+      },
+      [&](unsigned int n){
+        return n;
+      });
 
     ReinforcementLearning::For<ACTION_TYPE, REWARD_TYPE>(
-      [&](){return policy();},
-      [&](ACTION_TYPE T){return env(T);},
-      [&](REWARD_TYPE r, ACTION_TYPE a, unsigned long long i) {
+      [&](unsigned long long i, REWARD_TYPE r){return policy();},
+      [&](unsigned long long i, ACTION_TYPE a){return env(a);},
+      [&](unsigned long long i, REWARD_TYPE r, ACTION_TYPE a) {
         // update.
         const double INC = 1.0;
         auto mean = mean_1_14(Q[a], r, Q[a], Q[a] + INC);
